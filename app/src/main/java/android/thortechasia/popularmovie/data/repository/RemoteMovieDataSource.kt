f05c4eeeb9d9b7c18979bce62a30ecc9848ebf78
riefist <muhamadarief3101@gmail.com>
2018-12-17 09:35:31 +0800

    change to observable remote source

--- app/src/main/java/android/thortechasia/popularmovie/data/repository/RemoteMovieDataSource.kt
+++ app/src/main/java/android/thortechasia/popularmovie/data/repository/RemoteMovieDataSource.kt
@@ -1,16 +1,12 @@
 package android.thortechasia.popularmovie.data.repository
 
-import android.thortechasia.popularmovie.data.lokal.PopularMovieEntity
 import android.thortechasia.popularmovie.data.remote.PopularMovieModel
 import android.thortechasia.popularmovie.network.ApiService
 import io.reactivex.Observable
-import io.reactivex.Single
-import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.schedulers.Schedulers
 
 class RemoteMovieDataSource(val apiService: ApiService){
 
-    fun getPopularMovies(): Single<PopularMovieModel.Response> {
+    fun getPopularMovies(): Observable<PopularMovieModel.Response> {
         return apiService.getPopularMovies()
     }
 }
--- app/src/main/java/android/thortechasia/popularmovie/network/ApiService.kt
+++ app/src/main/java/android/thortechasia/popularmovie/network/ApiService.kt
@@ -4,6 +4,7 @@ package android.thortechasia.popularmovie.network
 import android.thortechasia.popularmovie.BuildConfig
 import android.thortechasia.popularmovie.data.remote.PopularMovieModel
 import android.thortechasia.popularmovie.utils.Constants.Companion.POPULAR_MOVIES
+import io.reactivex.Observable
 import io.reactivex.Single
 import retrofit2.http.GET
 import retrofit2.http.Query
@@ -12,6 +13,6 @@ interface ApiService {
 
     @GET(POPULAR_MOVIES)
     fun getPopularMovies(@Query("api_key") api_key : String = BuildConfig.API_KEY)
-        : Single<PopularMovieModel.Response>
+        : Observable<PopularMovieModel.Response>
 
 }
